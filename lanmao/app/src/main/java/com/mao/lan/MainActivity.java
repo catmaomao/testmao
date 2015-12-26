c4a68f3b40b157499ca3fbabc589327874148397
mashaohu <15711279225@163.com>
2015-12-26 21:03:10 +0800

    获取view对象

--- lanmao/app/src/main/java/com/mao/lan/MainActivity.java
+++ lanmao/app/src/main/java/com/mao/lan/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        View viewById = findViewById(R.id.tv_test);
         setSupportActionBar(toolbar);
 
         /*注释*/
