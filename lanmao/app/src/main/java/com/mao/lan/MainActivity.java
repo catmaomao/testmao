a4b1c2e02cf5551d0e87759a095fdde9f02895c4
mashaohu <15711279225@163.com>
2015-12-27 21:21:26 +0800

    version 2.0

--- lanmao/app/src/main/java/com/mao/lan/MainActivity.java
+++ lanmao/app/src/main/java/com/mao/lan/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        /*v2.0*/
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
